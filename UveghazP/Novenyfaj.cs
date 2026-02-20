084b60d25d5ef3cf0ab344a14bae2bf515b1eb2e
Petrik Diak <diak@petrik.hu>
2026-02-20 09:27:44 +0100

    novenyfaj osztaly kesz

--- UveghazP/Novenyfaj.cs
+++ UveghazP/Novenyfaj.cs
@@ -8,5 +8,34 @@ namespace UveghazP
 {
 	internal class Novenyfaj
 	{
-	}
+		string nev;
+		int nedvessegtartalom; //100 ideális
+		int egeszsegszint; //100 max, 60 jajj jajj
+		int optimalissuruseg;
+
+		public Novenyfaj(string nev, int optimalissuruseg)
+		{
+			this.nev = nev;
+			this.optimalissuruseg = optimalissuruseg;
+			this.nedvessegtartalom = 100;
+			this.egeszsegszint = 100;
+		}
+
+		public string Nev { get => nev; set => nev = value; }
+		public int Nedvessegtartalom { get => nedvessegtartalom; set => nedvessegtartalom = value; }
+		public int Egeszsegszint { get => egeszsegszint; set => egeszsegszint = value; }
+		public int Optimalissuruseg { get => optimalissuruseg; set => optimalissuruseg = value; }
+	
+		public string Azonosito
+		{
+			get
+			{
+				return this.nev.Substring(0, 3);
+			}
+		}
+		public override string ToString()
+		{
+			return $"{this.nev} - Nedvességtartalom: {this.nedvessegtartalom}%, Egészségszint: {this.egeszsegszint}% ideális egyedszám: ({this.optimalissuruseg})";
+		}
+	} 
 }
