e4b099772ebc1d522cb19f32aaf55e3569a3c617
Petrik Diak <diak@petrik.hu>
2026-02-20 10:41:48 +0100

    novenyek tesztelese megtortent

--- UveghazP/Adattar.cs
+++ UveghazP/Adattar.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +9,22 @@ namespace UveghazP
 {
 	internal class Adattar
 	{
+		List<Novenyfaj> novenyek;
+		public Adattar()
+		{
+			novenyek = new List<Novenyfaj>();
+		}
+		public void Ujnoveny(Novenyfaj noveny)
+		{
+			novenyek.Add(noveny);
+		}
+		public void Novenyeklistazasa()
+		{
+			foreach (var item in novenyek)
+			{
+				Console.WriteLine(item);
+			}
+		}
 
 	}
 }
--- UveghazP/Program.cs
+++ UveghazP/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-        }
+            Adattar adattar = new Adattar();
+			adattar.Ujnoveny(new Novenyfaj("Uborka", 5));
+			adattar.Ujnoveny(new Novenyfaj("Sárgarépa", 6));
+			adattar.Ujnoveny(new Novenyfaj("Burgonya", 4));
+			adattar.Ujnoveny(new Novenyfaj("Hagyma", 7));
+			adattar.Ujnoveny(new Novenyfaj("fokhagyma", 8));
+			adattar.Ujnoveny(new Novenyfaj("Saláta", 5));
+			adattar.Ujnoveny(new Novenyfaj("Kukorica", 3));
+			adattar.Ujnoveny(new Novenyfaj("Borsó", 6));
+			adattar.Ujnoveny(new Novenyfaj("Bab", 5));
+			adattar.Ujnoveny(new Novenyfaj("Káposzta", 4));
+			adattar.Ujnoveny(new Novenyfaj("Karfiol", 4));
+			adattar.Ujnoveny(new Novenyfaj("Brokkoli", 4));
+			adattar.Ujnoveny(new Novenyfaj("Cukkini", 5));
+			adattar.Ujnoveny(new Novenyfaj("Spenót", 7));
+
+			adattar.Novenyeklistazasa();
+		}
     }
 }
